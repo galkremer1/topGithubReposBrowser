31e7226a76bd4bf922a22d2a6d9d567d9cab8e15
galkremer1 <galkremer1@walla.co.il>
2023-04-12 16:10:46 -0500

    Making sure that commit author is defined.

--- src/app/components/Commit.tsx
+++ src/app/components/Commit.tsx
@@ -6,16 +6,18 @@ export function Commit({ commit }: { commit: GitHubCommit }) {
     <div className="flex flex-row items-center border-b-2 cursor-pointer border-gray-300 py-4 bg-gray-50 hover:bg-gray-200">
       <div className="w-1/3 p-4">
         <div className="flex space-between font-medium text-gray-700">
-          <a href={commit.author.html_url} target="_blank">
-            <Image
-              className="w-5 h-5 rounded-full mr-4"
-              data-testid="avatar"
-              src={commit.author.avatar_url}
-              alt={commit.author.login}
-              width={20}
-              height={20}
-            />
-          </a>
+          {commit.author && (
+            <a href={commit.author.html_url} target="_blank">
+              <Image
+                className="w-5 h-5 rounded-full mr-4"
+                data-testid="avatar"
+                src={commit.author.avatar_url}
+                alt={commit.author.login}
+                width={20}
+                height={20}
+              />
+            </a>
+          )}
           <p title={commit.commit.author.name} className="truncate">
             {commit.commit.author.name}
           </p>
